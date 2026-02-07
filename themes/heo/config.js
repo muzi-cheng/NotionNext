3b3dffb1bfc40a3793d91916064a70b350351e75
CHENG LI <58689883+muzi-cheng@users.noreply.github.com>
2026-02-08 02:26:54 +0800

    Update hero section titles and links

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,10 +20,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: '0rz.li',
+  HEO_HERO_TITLE_4: '人生得意须尽欢',
+  HEO_HERO_TITLE_5: '莫使金樽空对月',
+  HEO_HERO_TITLE_LINK: 'https://0rz.li',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
